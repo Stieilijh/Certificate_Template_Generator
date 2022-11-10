c27691e662b8ef14cb681ada5bf5c66ac229e694
Stieilijh <saitushar02@gmail.com>
2022-11-10 19:33:39 +0530

    clean up

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/stieilijh/CERTIFICATE_TEMPLATE_GENERATOR/",
+  base: "/Certificate_Template_Generator/",
 });
